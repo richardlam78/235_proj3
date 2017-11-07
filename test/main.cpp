f14948d5cd732f0f27a93346066d0bc0e174cea4
richardlam78 <richard.lam78@myhunter.cuny.edu>
2017-11-06 22:01:27 -0500

    best working draft, but still has couts in cpp files for testing

--- test/main.cpp
+++ test/main.cpp
@@ -8,27 +8,27 @@ using std::endl;
 
 void refineInput(string& input)
 {
+  // string input = expression;
   int i = 0;
-  while(i < input.size()) {
+  while(i < input.size() - 1) {
     while (isdigit(input[i]) || input[i] == '.' || input[i] == ' ') {
       ++i;
     }
 
     input = input.substr(0,i) + ' ' + input[i] + ' ' + input.substr(i+1);
     i += 2;
-    cout << input << endl;
-
   }
+  input = "(" + input + ")";
+  // return input;
 }
 
 // stringstreams best for converting individual strings to numbers
 int main()
 {
   string text = "(3.4+4)";
-  cout << text << endl;
+  cout << "raw text: " << text << endl;
   refineInput(text);
-  // text = text.substr(0,2) + " " + text.substr(2);
-  cout << text << endl;
+  cout << "new text: " << text << endl;
   /*
   istringstream s_in(text);
 
